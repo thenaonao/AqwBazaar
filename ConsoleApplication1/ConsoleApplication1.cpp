d6c23265e95c105389d6d99867695fa5172630dd
AqwerfNet <coucou48500@yahoo.fr>
2020-03-17 07:26:22 +0900

    Basic I/O

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main()
 {
-    std::cout << "test" << std::endl;
+    std::fstream dataReader;
+    std::string Output;
+    dataReader.open("Data.MF4", std::ios::out | std::ios::in | std::ios::binary);
+
+    if (dataReader.is_open()) {
+        for (int i = 0; i < 64; i++) {
+            dataReader << i << std::endl;
+        }
+        dataReader.seekg(0);
+        for (int j = 0; j < 64; j++) {
+            dataReader >> Output;
+            std::cout << Output << std::endl;
+        }
+
+        dataReader.close();
+    }
+    else {
+        std::cout << "Error while opening the data" << std::endl;
+        return -1;
+    }
+    //std::cout << "test" << std::endl;
 }
